entropy loss, reduce_mean, and numerical stability fixes

pg_network.py
@@ -49,20 +49,29 @@ class PolicyGradientNetwork:
 
         self.g = tf.placeholder(tf.float32, shape=[None, ])
         print('g %s' % (self.g.get_shape()))
-
-        good_probabilities = tf.reduce_sum(tf.multiply(self.y, self.selected_action), reduction_indices=[1])
+        
+        stable_y = self.y + 1e-8
+        good_probabilities = tf.reduce_sum(tf.multiply(stable_y, self.selected_action), reduction_indices=[1])
         print('good_probabilities %s' % (good_probabilities.get_shape()))
         log_probabilities = tf.log(good_probabilities)
-        scaled_log_p = tf.multiply(self.g, log_probabilities)
-        self.loss = -tf.reduce_sum(scaled_log_p)
+        self.loss_reinforce = -tf.multiply(self.g, log_probabilities)
+        self.loss_reinforce_mean = tf.reduce_mean(self.loss_reinforce)
         
-        if args.use_rms_prop:
-            #optimizer = tf.train.RMSPropOptimizer(args.learning_rate, decay=.95, epsilon=.01)
-            optimizer = GradientClippingOptimizer(tf.train.RMSPropOptimizer(args.learning_rate, decay=.95, epsilon=.01))
-        else:
+        self.loss_entropy = -tf.reduce_sum(stable_y * tf.log(stable_y), 1)
+        self.loss_entropy_mean = tf.reduce_mean(self.loss_entropy)
+        
+        self.loss_total = tf.reduce_mean(self.loss_reinforce - args.entropy_loss_factor * self.loss_entropy)
+        
+        if args.optimizer == 'rms':
+            learning_rate = args.learning_rate if args.learning_rate is not None else .00025
+            optimizer = tf.train.RMSPropOptimizer(learning_rate, decay=.95, epsilon=.01)
+        elif args.optimizer == 'adam':
+            learning_rate = args.learning_rate if args.learning_rate is not None else .001
             optimizer = tf.train.AdamOptimizer(args.learning_rate)
+
+        optimizer = GradientClippingOptimizer(optimizer)
         
-        self.train_step = optimizer.minimize(self.loss)
+        self.train_step = optimizer.minimize(self.loss_total)
 
         self.saver = tf.train.Saver(max_to_keep=25)
 
@@ -147,9 +156,9 @@ class PolicyGradientNetwork:
         # one hot encode the selected actions
         selected_action = [np.eye(self.numActions)[i] for i in selected_action]
 
-        _, loss = self.sess.run([self.train_step, self.loss], feed_dict={
+        _, loss, loss_r, loss_h = self.sess.run([self.train_step, self.loss_total, self.loss_reinforce_mean, self.loss_entropy_mean], feed_dict={
             self.x: x,
             self.selected_action: selected_action,
             self.g: g
         })
-        return loss
+        return loss, loss_r, loss_h

play_atari.py
@@ -19,16 +19,14 @@ parser.add_argument("--observation-steps", type=int, default=50000, help="train 
 parser.add_argument("--model", help="tensorflow model checkpoint file to initialize from")
 parser.add_argument("--games-per-epoch", type=int, default=100, help="Number of games to play per training epoch (default 100)")
 parser.add_argument("--learning-rate", type=float, help="Learning rate (default .001 for Adam, .00025 for RMSProp)")
-parser.add_argument("--training-passes-per-epoch", type=int, default=1, help="How many passes over training data to make per epoch (default 1)")
-parser.add_argument("--use-rms-prop", action='store_true', default=False, help="Use the RMSPropOptimizer instead of Adam")
+parser.add_argument("--training-passes-per-epoch", type=int, default=4, help="How many passes over training data to make per epoch (default 1)")
+parser.add_argument("--optimizer", choices=['rms', 'adam'], default="rms", help="Optimizer (rms or adam, default is rms)")
 parser.add_argument("--gamma", type=float, default=.99, help="Learning rate (default .99)")
 parser.add_argument("--batch-size", type=int, default=20, help="Batch size for training (default 20)")
+parser.add_argument("--entropy-loss-factor", type=float, default=0.01, help="Entropy loss regularization factor (0.01)")
 parser.add_argument("rom", help="rom file to run")
 
 args = parser.parse_args()
-
-if args.learning_rate is None:
-    args.learning_rate = .00025 if args.use_rms_prop else .001
 
 print('Arguments: %s' % (args))
 
@@ -103,12 +101,19 @@ def trainEpoch():
     
       print("Training pass %d of %d with %d batches..." % (training_pass+1, args.training_passes_per_epoch, len(batches)), end='')
       sys.stdout.flush()
-      total_loss = 0
+      loss = 0
+      loss_r = 0
+      loss_h = 0
       for i, batch in enumerate(batches):
           x, y, returns = zip(*batch)
-          total_loss += pgn.train(x, y, returns)
-      average_loss = total_loss / len(batches)
-      print(" Done (ave loss: %f)" % average_loss)
+          l, l_r, l_h = pgn.train(x, y, returns)
+          loss += l
+          loss_r += l_r
+          loss_h += l_h
+      ave_loss = loss / len(batches)
+      ave_loss_r = loss_r / len(batches)
+      ave_loss_h = loss_h / len(batches)
+      print(" Done (loss: %f   loss_r: %f   loss_h: %f   (prob %f%%))" % (ave_loss, ave_loss_r, ave_loss_h, np.exp(-ave_loss_r)))
     
 while True:
    trainEpoch()