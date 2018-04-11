5fa924e7435eb0ef64ba0a8cf599f9558c00685a
Garrick Toubassi <gtoubassi@gmail.com>
2018-04-10 17:19:06 -0700

    Add total steps to logstats

--- logstats.sh
+++ logstats.sh
@@ -9,6 +9,9 @@ cat $1 | awk '$1=="Episode" {SUM=SUM+$6;COUNT=COUNT+1} $1=="Training"&& SUM>0 {p
 echo -n "Number of parameter updates:  "
 grep Train $1 | awk '{print $7}'  | st | tail -1 | awk '{print $4}'
 
+echo -n "Total steps (=4 frames) executed: "
+grep Episode $1 | awk '{print $7}' | tr -d '(' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{printf "%'"'"'d\n",  SUM/4}'
+
 echo -n "Initial loss: "
 grep loss: $1  | head -1 | tr -d '()' | awk '{print $11}'
 
