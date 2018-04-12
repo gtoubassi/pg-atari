992c05600d38eb0ce91065a72d76d784c7e6df25
Garrick Toubassi <gtoubassi@gmail.com>
2018-04-12 10:26:13 -0700

    format tweaks for logstats

--- logstats.sh
+++ logstats.sh
@@ -7,7 +7,7 @@ echo  "Last 20 epoch ave scores:"
 cat $1 | awk '$1=="Episode" {SUM=SUM+$6;COUNT=COUNT+1} $1=="Training"&& SUM>0 {printf "%f\n",SUM/COUNT;SUM=0;COUNT=0}' | tail -20
 
 echo -n "Number of parameter updates:  "
-grep Train $1 | awk '{print $7}'  | st | tail -1 | awk '{print $4}'
+grep Train $1 | awk '{print $7}'  | st --format=%d | tail -1 | awk '{print $4}'
 
 echo -n "Total steps (=4 frames) executed: "
 grep Episode $1 | awk '{print $7}' | tr -d '(' | awk '{SUM=SUM+$1;COUNT=COUNT+1} END{printf "%'"'"'d\n",  SUM/4}'
